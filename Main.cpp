f5edf11a9cbe0284d28e42d5bc470fe30bb27518
TechnoBulldog <techno.bulldog@gmail.com>
2011-02-03 14:23:02 -0600

    Ok, let's try this again....

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+// This file does not fall under any licenses.
+
+#include "Breezy/BreezySample.h"
+
+int main(int argc, char** argv)
+{
+	Breezy::Sample * app = new Breezy::Sample();
+	app->Start();
+	return 0;
+}
