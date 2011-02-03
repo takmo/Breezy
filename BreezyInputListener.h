190f9d0b667d1ec1260613c516cfbfce643ba194
TechnoBulldog <techno.bulldog@gmail.com>
2011-02-02 19:30:27 -0600

    Changed a bit of the license as well as fixed a typo.

--- BreezyInputListener.h
+++ BreezyInputListener.h
@@ -1,7 +1,7 @@
 /*
 File: BreezyInputListener.h
 
-Copyright (c) 2010 Joshua Dolifka
+Copyright (c) 2011 TechnoBulldog
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
